fe4c105ba668aef84095f369f31d078903ec57f5
IAmFunkyFrog <st085873@student.spbu.ru>
2021-08-14 23:52:41 +0300

    Added predicate

--- src/main/kotlin/expressions/evaluators/specialForms/predicates/IfPredicate.kt
+++ src/main/kotlin/expressions/evaluators/specialForms/predicates/IfPredicate.kt
@@ -1,14 +1,15 @@
 package expressions.evaluators.specialForms.predicates
 
 import environment.Environment
+import expressions.evaluators.specialForms.If
 import expressions.evaluators.specialForms.SpecialForm
 
 class IfPredicate: Predicate {
     override fun check(expression: List<String>, environment: Environment): Boolean {
-        TODO("Not yet implemented")
+        return expression[0] == "if"
     }
 
     override fun getSpecialForm(expression: List<String>, environment: Environment): SpecialForm {
-        TODO("Not yet implemented")
+        return If(expression, environment)
     }
 }
