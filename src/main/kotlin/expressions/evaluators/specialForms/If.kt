1dd97ba33571a46b4274e8a358fbd67f7db549e8
IAmFunkyFrog <st085873@student.spbu.ru>
2021-08-18 17:50:23 +0300

    Refractoring

--- src/main/kotlin/expressions/evaluators/specialForms/If.kt
+++ src/main/kotlin/expressions/evaluators/specialForms/If.kt
@@ -11,7 +11,8 @@ class If(
     override fun evaluate(): Pair<List<String>, Environment> {
         val conditionResult = Expression(expression[1], environment).evaluate().first
 
-        return if(SelfEvaluating.isFalse(conditionResult)) Expression(expression[3], environment).evaluate()
-        else Expression(expression[2], environment).evaluate()
+        return if(!SelfEvaluating.isFalse(conditionResult)) Expression(expression[2], environment).evaluate()
+        else if(expression.size > 3) Expression(expression[3], environment).evaluate()
+        else Pair(SelfEvaluating.nil, environment)
     }
 }
