035855237c863822bdbdc57fb28224d765a1214e
IAmFunkyFrog <st085873@student.spbu.ru>
2021-08-18 17:32:24 +0300

    Added nil primitive procedures

---
+++ src/main/kotlin/environment/primitiveProcedures/isNil.kt
@@ -0,0 +1,21 @@
+package environment.primitiveProcedures
+
+import environment.Environment
+import expressions.evaluators.Expression
+import expressions.evaluators.specialForms.SelfEvaluating
+
+class isNil(): PrimitiveProcedure {
+    override val name: String = "nil?"
+
+    override fun evaluate(expression: List<String>, environment: Environment): List<String> {
+        if(expression.size > 2) throw Exception("Excepted 1 parameters but given ${expression.size - 1}")
+        if(expression[0] != name) throw Exception("Bad procedure name given to $name evaluator")
+
+        val parameter1 = Expression(expression[1], environment).evaluate().first
+
+        return ArrayList<String>().apply {
+            if(SelfEvaluating.isNil(parameter1)) add("true")
+            else add("false")
+        }
+    }
+}
