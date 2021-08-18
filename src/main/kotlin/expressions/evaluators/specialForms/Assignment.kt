0ae7b893b75b12494be7825a5bb72081169fd3bc
IAmFunkyFrog <st085873@student.spbu.ru>
2021-08-18 20:58:23 +0300

    Assignment special form implementation

--- src/main/kotlin/expressions/evaluators/specialForms/Assignment.kt
+++ src/main/kotlin/expressions/evaluators/specialForms/Assignment.kt
@@ -1,13 +1,22 @@
 package expressions.evaluators.specialForms
 
 import environment.Environment
+import expressions.evaluators.Expression
 import expressions.evaluators.specialForms.SpecialForm
+import expressions.evaluators.specialForms.predicates.LambdaPredicate
+import kotlin.math.exp
 
 class Assignment(
     expression: List<String>,
     environment: Environment
 ) : SpecialForm(expression, environment) {
     override fun evaluate(): Pair<List<String>, Environment> {
-        TODO("Not yet implemented")
+        val lambdaPredicate = LambdaPredicate()
+        val evaluatedValue = Expression(expression[2], environment).evaluate()
+
+        if(lambdaPredicate.check(evaluatedValue.first, environment)) environment.setProcedure(expression[1], evaluatedValue.first, evaluatedValue.second)
+        else environment.setVariable(expression[1], evaluatedValue.first)
+
+        return Pair(evaluatedValue.first, environment)
     }
 }
